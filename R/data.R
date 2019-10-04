95ef7b9183a1dad85e45d3b941d70bfc783c4e47
Alex Rix <alexanderjrix@gmail.com>
2019-10-04 09:18:42 -0400

    Add example dataset

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Synthetic example data for higlasso
+#'
+#' X1 and X2 have (degree 3) main effects, and interact with each other.
+#' @format A synthetic data.frame with 500 observations on 11 variables:
+#' \describe{
+#'   \item{y}{Continuous response.}
+#'   \item{X1-X10}{Covariates.}
+#' }
+"higlasso.df"
